ef22367060b6ae7951c6216ad4e83a9c55de069a
Thomas Kieslich <post@thomaskieslich.de>
2024-08-31 14:08:56 +0200

    Add 10.4

---
+++ versions/10.4/db-fixtures.sql
@@ -0,0 +1,2 @@
+# Add your needed SQL Queries here!
+# UPDATE `sys_template` SET `include_static_file` = 'EXT:fluid_styled_content/Configuration/TypoScript/,EXT:fluid_styled_content/Configuration/TypoScript/Styling/,EXT:tkflatmanager/Configuration/TypoScript,EXT:powermail/Configuration/TypoScript/Main,EXT:theme_web/Configuration/TypoScript/' WHERE `sys_template`.`uid` = 1;
