dbac9035d0db0847c2eed454bb92ad9b6d81acf7
Thomas Kieslich <post@thomaskieslich.de>
2024-08-31 14:44:07 +0200

    Update t3upgrader

---
+++ versions/11.5/update-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set -ex
+
+# Get base Settings
+source "./t3upgrader/.env"
+
+# get Version Settings
+source ".env"
+
+# 11.5
+vendor/bin/typo3cms -nq install:generatepackagestates
+vendor/bin/typo3cms -nq install:fixfolderstructure
+vendor/bin/typo3cms -nq install:extensionsetupifpossible
+vendor/bin/typo3cms -nq language:update
+
+vendor/bin/typo3cms -nq database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db-fixtures.sql"
+vendor/bin/typo3 -nq upgrade:run
+vendor/bin/typo3cms -nq database:updateschema
+vendor/bin/typo3cms -nq cache:flush
