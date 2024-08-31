ccc8b48bc85e23456ad28a0bc10a246fbac242e7
Thomas Kieslich <post@thomaskieslich.de>
2024-08-31 13:04:47 +0200

    Update t3upgrader

--- versions/8.7/update-script.sh
+++ versions/8.7/update-script.sh
@@ -12,9 +12,9 @@ source ".env"
 vendor/bin/typo3cms install:generatepackagestates
 vendor/bin/typo3cms install:fixfolderstructure
 vendor/bin/typo3cms install:extensionsetupifpossible
-#  vendor/bin/typo3 lang:language:update
+vendor/bin/typo3cms language:update
 
-#  vendor/bin/typo3cms database:updateschema
-#  vendor/bin/typo3cms upgrade:all
+vendor/bin/typo3cms database:updateschema
+vendor/bin/typo3cms upgrade:all
 vendor/bin/typo3cms database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db-fixtures.sql"
 vendor/bin/typo3cms cache:flush
