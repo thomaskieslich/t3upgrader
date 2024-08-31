b4f82bb6fff8a678f5fbad7effaaf0506df73483
Thomas Kieslich <post@thomaskieslich.de>
2024-08-31 13:44:54 +0200

    Update t3upgrader

--- t3upgrade.sh
+++ t3upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set -x
-set -e
+# set -e
 
 # Get base Settings
 if [ -f ${BASH_SOURCE%/*}/.env ]; then
--- versions/8.7/update-script.sh
+++ versions/8.7/update-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+# set -ex
 
 # Get base Settings
 source "./t3upgrader/.env"
@@ -14,7 +14,7 @@ vendor/bin/typo3cms -nq install:fixfolderstructure
 vendor/bin/typo3cms -nq install:extensionsetupifpossible
 vendor/bin/typo3cms -nq language:update
 
-#vendor/bin/typo3cms -nq database:updateschema
-vendor/bin/typo3cms -nq upgrade:all
 vendor/bin/typo3cms -nq database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db-fixtures.sql"
+vendor/bin/typo3cms -nq upgrade:all
+vendor/bin/typo3cms -nq database:updateschema
 vendor/bin/typo3cms -nq cache:flush
