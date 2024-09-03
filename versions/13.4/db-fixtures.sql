190d72b233deb247234736fc4c328a4d01deb447
Thomas Kieslich <thomaskieslich@users.noreply.github.com+>
2024-09-03 22:29:12 +0200

    t3upgrader: update 13.4 Upgrade

---
+++ versions/13.4/db-fixtures.sql
@@ -0,0 +1,5 @@
+# Update root Template
+#UPDATE `sys_template` SET `include_static_file` = 'EXT:fluid_styled_content/Configuration/TypoScript/,EXT:fluid_styled_content/Configuration/TypoScript/Styling/,EXT:tkflatmanager/Configuration/TypoScript,EXT:powermail/Configuration/TypoScript/Main,EXT:theme_web/Configuration/TypoScript/' WHERE `sys_template`.`uid` = 1;
+
+
+ALTER TABLE `fe_users` CHANGE `image` `image` INT UNSIGNED DEFAULT 0 NULL
