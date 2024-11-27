1a955d6fb48a70043e903bed1ef943b725164d35
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2024-11-27 21:33:45 +0100

    add more CLI Commands

--- versions/11.5/update-script.sh
+++ versions/11.5/update-script.sh
@@ -17,3 +17,5 @@ vendor/bin/typo3 -nq database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db
 vendor/bin/typo3 -nq upgrade:run
 vendor/bin/typo3 -nq database:updateschema
 vendor/bin/typo3 -nq cache:flush
+vendor/bin/typo3 -nq referenceindex:update
+vendor/bin/typo3 -nq cache:warmup
