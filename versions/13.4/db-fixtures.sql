a8b51841e9327f77d762ab253ad12f3ec677b776
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2025-03-21 16:15:59 +0100

    TYPO3 13: disable sys_templates

--- versions/13.4/db-fixtures.sql
+++ versions/13.4/db-fixtures.sql
@@ -2,6 +2,7 @@
 #UPDATE `sys_template` SET `include_static_file` = 'EXT:fluid_styled_content/Configuration/TypoScript/,EXT:fluid_styled_content/Configuration/TypoScript/Styling/,EXT:tkflatmanager/Configuration/TypoScript,EXT:powermail/Configuration/TypoScript/Main,EXT:theme_web/Configuration/TypoScript/' WHERE `sys_template`.`uid` = 1;
 
 # Clean sys_template if using Site Sets
+UPDATE `sys_template` SET `hidden` = '1';
 # TRUNCATE TABLE `sys_template`;
 
 # ALTER TABLE `fe_users` CHANGE `image` `image` INT UNSIGNED DEFAULT 0 NULL
