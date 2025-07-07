6a57159dfffec91340e6829736742288ce57f149
Philipp Gollmer <pg@davitec.de>
2025-07-07 10:52:09 +0200

    Improve echo output visualization

--- t3upgrade.sh
+++ t3upgrade.sh
@@ -30,37 +30,37 @@ splitVersions() {
 }
 
 initUpgrade() {
-  echo "Create DB Backup Dir"
+  echoStep "Create DB Backup Dir \n mkdir -p ${DB_BACKUP_DIR}/${version}"
   mkdir -p ${DB_BACKUP_DIR}/${version}
 
   if [ "${version}" = "${BASE_CMS_VERSION}" ]; then
-    echo "Copy Production DB"
+    echoStep "Copy Production DB \n cp ${BASE_DB} ${DB_BACKUP_DIR}/${version}/typo3-db-${version}.sql.gz"
     cp ${BASE_DB} ${DB_BACKUP_DIR}/${version}/typo3-db-${version}.sql.gz
   else
-    echo "Export current DB ..."
+    echoStep "Export current DB ... \n ddev export-db -z -f ${DB_BACKUP_DIR}/${version}/typo3-db-${version}.sql.gz"
     ddev export-db -z -f ${DB_BACKUP_DIR}/${version}/typo3-db-${version}.sql.gz
   fi
 
-  echo "Checkout ${version} branch ..."
+  echoStep "Checkout ${version} branch ... \n git checkout ${BRANCH_PREFIX}-${version}"
   git checkout ${BRANCH_PREFIX}-${version}
 }
 
 importCleanDB() {
-  echo "Stop DDEV ..."
+  echoStep "Stop DDEV ... \n ddev stop"
   ddev stop
 
-  echo "Clean DDEV ..."
+  echoStep "Clean DDEV ... \n ddev delete --omit-snapshot --yes"
   ddev delete --omit-snapshot --yes
 
-  echo "Start DDEV ..."
+  echoStep "Start DDEV ... \n ddev start"
   ddev start
 
-  echo "Import previous DB ..."
+  echoStep "Import previous DB ... \n ddev import-db --file=${DB_BACKUP_DIR}/${version}/typo3-db-${version}.sql.gz"
   ddev import-db --file=${DB_BACKUP_DIR}/${version}/typo3-db-${version}.sql.gz
 }
 
 cleanFolders() {
-  echo "Clean Folders ..."
+  echoStep "Clean Folders ..."
   rm -rf ./public/typo3conf/ext && rm -rf ./public/typo3conf/l10n
   rm -rf ./public/_assets && rm -rf ./public/typo3 && rm -rf ./public/index.php
   rm -rf ./var && rm -rf ./public/typo3temp
@@ -77,21 +77,26 @@ composerInstall() {
 }
 
 updateTYPO3() {
-  echo "Update TYPO3"
+  echoStep "Update TYPO3 \n ddev exec ${VERSIONS_DIR}/${version}/update-script.sh"
   ddev exec ${VERSIONS_DIR}/${version}/update-script.sh
 }
 
 finishUpgrade() {
-  echo "Export current DB ..."
+  echoStep "Export current DB ... \n ddev export-db -z -f ${DB_BACKUP_DIR}/${version}/typo3-db-${version}-final.sql.gz"
   ddev export-db -z -f ${DB_BACKUP_DIR}/${version}/typo3-db-${version}-final.sql.gz
 
   # If not last version, stop DDEV and continue with next version
   if [ "${version}" != "${LAST_VERSION}" ]; then
-    echo "DDEV Stop ..."
+    echoStep "DDEV Stop ... \n ddev stop"
     ddev stop
   else
-    echo "Upgrade finished!"
+    echoStep "Upgrade finished!"
   fi
+}
+
+# issue echo commands with more visual means
+echoStep() {
+    echo -e "\n\033[1;34m==> $1\033[0m\n"
 }
 
 # Loop through versions and upgrade step by step
