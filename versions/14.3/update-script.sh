86819afa90b33eed00b36f683fd32909f17d14ba
Philipp Gollmer <pg@davitec.de>
2025-11-29 16:33:40 +0100

    Task: Add 14.3 to versions

---
+++ versions/14.3/update-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# set -ex
+
+# Get base Settings
+source "./t3upgrader/.env"
+
+# get Version Settings
+source ${ROOT_ENV_FILE}
+
+# 14.3
+vendor/bin/typo3 -nq install:fixfolderstructure
+vendor/bin/typo3 -nq install:extensionsetupifpossible
+vendor/bin/typo3 -nq language:update
+
+vendor/bin/typo3 -nq database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db-fixtures.sql"
+vendor/bin/typo3 -nq upgrade:run
+vendor/bin/typo3 -nq database:updateschema
+vendor/bin/typo3 -nq cache:flush
+vendor/bin/typo3 -nq referenceindex:update
+vendor/bin/typo3 -nq cache:warmup
