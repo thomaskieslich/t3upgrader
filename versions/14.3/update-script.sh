78edcce887083da21296b5503cd2baa0507e02fe
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2025-11-29 21:12:41 +0100

    Update script adjustments for TYPO3 14.3 upgrade process

--- versions/14.3/update-script.sh
+++ versions/14.3/update-script.sh
@@ -9,13 +9,35 @@ source "./t3upgrader/.env"
 source ${ROOT_ENV_FILE}
 
 # 14.3
-vendor/bin/typo3 -nq install:fixfolderstructure
-vendor/bin/typo3 -nq install:extensionsetupifpossible
+# Repair folder structure ((typo3-console))
+#vendor/bin/typo3 -nq install:fixfolderstructure
+
+# Initialize extensions (typo3-cli)
+vendor/bin/typo3 -nq extension:setup
+
+# (typo3-console)
+#vendor/bin/typo3 -nq database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db-fixtures.sql"
+
+# (typo3-cli)
+vendor/bin/typo3 -nq upgrade:run
+
+# (typo3-console)
+#vendor/bin/typo3 -nq database:updateschema
+
+# (typo3-cli)
 vendor/bin/typo3 -nq language:update
 
-vendor/bin/typo3 -nq database:import <${VERSIONS_DIR}/${CURRENT_CMS_VERSION}/"db-fixtures.sql"
-vendor/bin/typo3 -nq upgrade:run
-vendor/bin/typo3 -nq database:updateschema
+# (typo3-cli)
 vendor/bin/typo3 -nq cache:flush
+
+# (typo3-cli)
 vendor/bin/typo3 -nq referenceindex:update
+
+# (typo3-cli)
 vendor/bin/typo3 -nq cache:warmup
+
+# (typo3-cli)
+vendor/bin/typo3 -nq fluid:cache:warmup
+
+# (typo3-cli)
+vendor/bin/typo3 -nq fluid:schema:generate
